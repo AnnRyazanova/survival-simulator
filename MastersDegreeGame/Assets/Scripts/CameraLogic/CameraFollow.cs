4c560b2b4accadcf30814c61e000f1056b19daaf
Vitaly Galaichuk <swissarmytowel@gmail.com>
2020-03-04 15:45:43 +0300

    Add camera target following

---
+++ MastersDegreeGame/Assets/Scripts/CameraLogic/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace CameraLogic
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField] private GameObject targetBody;
+        private Vector3 _cameraOffset;
+
+        private void Start() => _cameraOffset = transform.position - targetBody.transform.position;
+
+        void Update() {
+            if (targetBody != null) transform.position = targetBody.transform.position + _cameraOffset;
+        }
+    }
+}
