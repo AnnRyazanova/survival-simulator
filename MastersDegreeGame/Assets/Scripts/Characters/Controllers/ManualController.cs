690b525e73bdfbe1402c117a8f8ac9e70bfa0e49
Vitaly Galaichuk <swissarmytowel@gmail.com>
2020-03-04 14:10:27 +0300

    Add manual movement controller & a sample test scene. Import examples from  Joystick pack

---
+++ MastersDegreeGame/Assets/Scripts/Characters/Controllers/ManualController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+
+namespace Characters.Controllers
+{
+    public class ManualController : MonoBehaviour
+    {
+        [SerializeField] private float characterMovementSpeed = 5f;
+        [SerializeField] private float characterRotationSpeed = 0.1f;
+
+        private CharacterController _directionalController = null;
+        private Transform _mainCamera = null;
+        private Vector3 _moveDirection = Vector3.zero;
+
+        private float _currentSpeed = 0f;
+        private float _smoothingVelocity = 0f;
+        private const float SmoothingTime = 0.1f;
+
+        // Initialization should be made via Unity editor. Should be initialized from UI Canvas Joystick instance
+        public FixedJoystick directionalJoystick;
+
+        private void Start() {
+            _directionalController = GetComponent<CharacterController>();
+            if (Camera.main != null) _mainCamera = Camera.main.transform;
+        }
+
+        private void Update() {
+            Move();
+        }
+
+        private void Move() {
+            var horizontalInput = directionalJoystick.Horizontal;
+            var verticalInput = directionalJoystick.Vertical;
+
+            CalculateSpeed(horizontalInput, verticalInput);
+            CalculateMovementDirection(horizontalInput, verticalInput,
+                _mainCamera.forward.normalized, _mainCamera.right.normalized);
+
+            // Turning the character to face in direction of movement
+            if (_moveDirection != Vector3.zero) {
+                transform.rotation = Quaternion.Slerp(transform.rotation,
+                    Quaternion.LookRotation(_moveDirection), characterRotationSpeed);
+            }
+
+            _directionalController.Move(_moveDirection * (_currentSpeed * Time.deltaTime));
+        }
+
+        private void CalculateSpeed(float hInput, float vInput) {
+            var targetSpeed = characterMovementSpeed * Mathf.Sqrt(hInput * hInput + vInput * vInput);
+            _currentSpeed = Mathf.SmoothDamp(_currentSpeed, targetSpeed,
+                ref _smoothingVelocity, SmoothingTime);
+        }
+
+        private void CalculateMovementDirection(float hInput, float vInput,
+                                                Vector3 fwdCameraDirection, Vector3 sdwCameraDirection) {
+            _moveDirection = (fwdCameraDirection * vInput + sdwCameraDirection * hInput).normalized;
+            // Set Y coordinate to zero to prevent character from tilting along that axis
+            _moveDirection.y = 0;
+        }
+    }
+}
