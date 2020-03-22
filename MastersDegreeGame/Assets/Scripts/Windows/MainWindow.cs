6760d5c488cac6c0f4a317fc91d3c3d2944ac62d
aryazanova <aryazanova@gamenuts.ru>
2020-03-22 16:36:17 +0300

    - Added ui-elements in MainWindow - Added background and fader prefabs for windows - Removed unused ui-elements - Changed TestWindow

--- MastersDegreeGame/Assets/Scripts/Windows/MainWindow.cs
+++ MastersDegreeGame/Assets/Scripts/Windows/MainWindow.cs
@@ -1,19 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainWindow : BaseWindow
 {
     public FixedJoystick Joystick;
+
+    [Header("Player indicators")]
+    [SerializeField] private Image _warmIndicator;
+    [SerializeField] private Image _healthIndicator; 
+    [SerializeField] private Image _energyIndicator; 
+    
     public override void Show()
     {
         base.Show();
         Init();
     }
-
-    public void OnTestBtnClick()
+    
+    public void OnPauseButtonClick()
     {
+        // Open SettingsWindow here
         TestController.Instance.ShowWindow();
+        Debug.Log($"[MainWindow::OnPauseButtonClick] You've clicked on PauseButton");
+    }
+
+    public void OnInventoryButtonClick()
+    {
+        // Open InventoryWindow here
+        Debug.Log($"[MainWindow::OnInventoryButtonClick] You've clicked on InventoryButton");
+    }
+
+    public void OnPickUpButtonClick()
+    {
+        Debug.Log($"[MainWindow::OnPickUpButtonClick] You've clicked on PickUpButton");
+    }
+
+    public void OnAttackButtonClick()
+    {
+        Debug.Log($"[MainWindow::OnAttackButtonClick] You've clicked on AttackButton");
     }
 
     protected override void OnShow() { }
