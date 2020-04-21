bb6682db3f5d3f178dcdfca2b4b0244f2d36e4fc
e_zavgar <zavgorodnev1996@gmail.com>
2020-04-21 19:45:42 +0300

    add death window

---
+++ MastersDegreeGame/Assets/Scripts/Windows/DeathWindow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathWindow : BaseWindow
+{
+    public void OnMenuButtonClick()
+    {
+        Debug.Log($"[TestWindow::OnMenuButtonClick] You've clicked on MenuButton");
+        Hide();
+        MainWindowController.Instance.HideWindow();
+        MenuController.Instance.ShowWindow();
+        SceneLoaderController.Instance.LoadMenuScene(false, false);
+    }
+}
