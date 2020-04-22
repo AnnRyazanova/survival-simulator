1753c8e7f6b43604f91e142b47a65160a72ffaeb
e_zavgar <zavgorodnev1996@gmail.com>
2020-04-22 13:26:05 +0300

    add NoiseMap

---
+++ MastersDegreeGame/Assets/Scripts/SceneGeneration/PerlinNoise/Noise.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise
+{
+    public static float[,] GenerateNoiseMap(int mapWidht, int mapHeight, float scale)
+    {
+        float[,] noiseMap = new float[mapWidht, mapHeight];
+
+        if (scale <= 0)
+        {
+            scale = 0.0001f;
+        }
+        
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x= 0; x < mapWidht; x++)
+            {
+                float SampleX = x / scale;
+                float SampleY = y / scale;
+
+                float perlinValue = Mathf.PerlinNoise(SampleX, SampleY);
+                noiseMap[x, y] = perlinValue;
+            }
+        }
+
+        return noiseMap;
+    }
+}
