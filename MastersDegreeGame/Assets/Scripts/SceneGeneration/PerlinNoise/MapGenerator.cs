96834deab69eb48316017e7a6d79ab0d6a8600c1
e_zavgar <zavgorodnev1996@gmail.com>
2020-04-23 00:01:59 +0300

    no message

--- MastersDegreeGame/Assets/Scripts/SceneGeneration/PerlinNoise/MapGenerator.cs
+++ MastersDegreeGame/Assets/Scripts/SceneGeneration/PerlinNoise/MapGenerator.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    public enum DrawMode
+    {
+        NoiseMap,
+        ColourMap
+    };
+
+    public DrawMode drawMode;
+
     public int mapWidht;
     public int mapHeight;
     public float noiseScale;
@@ -19,12 +27,39 @@ public class MapGenerator : MonoBehaviour
     
     public bool autoUpdate;
 
+    public TerrainType[] regions;
+
     public void GenerateMap()
     {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidht, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
+        Color[] colourMap = new Color[mapWidht * mapHeight];
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidht; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < regions.Length; i++)
+                {
+                    if (currentHeight <= regions[i].height)
+                    {
+                        colourMap[y * mapWidht + x] = regions[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
+        
         MapDisplay display = FindObjectOfType<MapDisplay>();
-        display.DrawNoiseMap(noiseMap);
+        if (drawMode == DrawMode.NoiseMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        }
+        else if (drawMode == DrawMode.ColourMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidht, mapHeight));
+        }
+        
 
     }
 
@@ -34,18 +69,26 @@ public class MapGenerator : MonoBehaviour
         {
             mapWidht = 1;
         }
-
         if (mapHeight < 1)
         {
             mapHeight = 1;
         }
         if (lacunarity < 1)
         {
-            mapHeight = 1;
+            lacunarity = 1;
         }
         if (octaves < 0)
         {
             octaves = 0;
         }
     }
+}
+
+[System.Serializable]
+
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color colour;
 }
