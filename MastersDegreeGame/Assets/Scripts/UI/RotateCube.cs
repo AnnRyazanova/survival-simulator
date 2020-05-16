e7d9827db07ff3dbe438254302c47826e4450936
aryazanova <aryazanova@gamenuts.ru>
2020-05-16 20:16:48 +0300

    Add Stencil Cube in menu

---
+++ MastersDegreeGame/Assets/Scripts/UI/RotateCube.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(0,.5f,0, Space.World);
+        
+        //transform.RotateAround(transform.position, transform.up, Time.deltaTime * 90f);
+    }
+}
