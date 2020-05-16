3b604503f9d8d9fe7ab801966bcdd62202db097b
aryazanova <aryazanova@gamenuts.ru>
2020-05-16 20:17:56 +0300

    fix

--- MastersDegreeGame/Assets/Scripts/UI/RotateCube.cs
+++ MastersDegreeGame/Assets/Scripts/UI/RotateCube.cs
@@ -5,7 +5,5 @@ public class RotateCube : MonoBehaviour
     void Update()
     {
         transform.Rotate(0,.5f,0, Space.World);
-        
-        //transform.RotateAround(transform.position, transform.up, Time.deltaTime * 90f);
     }
 }
